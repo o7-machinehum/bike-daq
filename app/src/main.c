6ba4400fdb9737cab94b7fac519918f8e9374961
Ryan Walker <ryan.cjw@gmail.com>
2023-06-01 15:36:14 -0700

    Custom characteristic working.

--- app/src/main.c
+++ app/src/main.c
@@ -25,9 +25,7 @@
 #include <zephyr/bluetooth/services/hrs.h>
 #include <zephyr/bluetooth/services/ias.h>
 
-#ifdef CONFIG_BOARD_ARDUINO_NANO_33_BLE
 #include <zephyr/usb/usb_device.h>
-#endif
 #include <zephyr/drivers/uart.h>
 
 #include <zephyr/bluetooth/bluetooth.h>
@@ -44,14 +42,53 @@
 #define LED0_NODE DT_ALIAS(led0)
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 
+/* Custom Service Variables */
+#define BT_UUID_CUSTOM_SERVICE_VAL \
+    BT_UUID_128_ENCODE(0x12345678, 0x1234, 0x5678, 0x1234, 0x56789abcdef0)
+
+static struct bt_uuid_128 vnd_uuid = BT_UUID_INIT_128(
+    BT_UUID_CUSTOM_SERVICE_VAL);
+
+static struct bt_uuid_128 vnd_enc_uuid = BT_UUID_INIT_128(
+    BT_UUID_128_ENCODE(0x12345678, 0x1234, 0x5678, 0x1234, 0x56789abcdef1));
+
 const struct device *const dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_console));
+
+#define VND_MAX_LEN 20
+
+static uint8_t indicating;
+static struct bt_conn *default_conn;
+
+#define VND_LONG_MAX_LEN 74
+
+static void pong_ccc_cfg_changed(const struct bt_gatt_attr *attr, uint16_t val)
+{
+	printk("val %u\n", val);
+
+	// remote_ready = (val == BT_GATT_CCC_NOTIFY);
+
+	// if (remote_ready && remote_handle) {
+	// 	pong_conn_ready(initiator);
+	// }
+}
+
+/* Vendor Primary Service Declaration */
+BT_GATT_SERVICE_DEFINE(vnd_svc,
+    BT_GATT_PRIMARY_SERVICE(&vnd_uuid),
+    BT_GATT_CHARACTERISTIC(&vnd_enc_uuid.uuid,
+                   BT_GATT_CHRC_NOTIFY,
+                   BT_GATT_PERM_NONE, NULL, NULL, NULL),
+	BT_GATT_CCC(pong_ccc_cfg_changed,
+		    BT_GATT_PERM_READ | BT_GATT_PERM_WRITE),
+);
 
 static const struct bt_data ad[] = {
     BT_DATA_BYTES(BT_DATA_FLAGS, (BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR)),
     BT_DATA_BYTES(BT_DATA_UUID16_ALL,
               BT_UUID_16_ENCODE(BT_UUID_HRS_VAL),
               BT_UUID_16_ENCODE(BT_UUID_BAS_VAL),
-              BT_UUID_16_ENCODE(BT_UUID_DIS_VAL))
+              BT_UUID_16_ENCODE(BT_UUID_CTS_VAL)),
+    BT_DATA_BYTES(BT_DATA_UUID128_ALL, BT_UUID_CUSTOM_SERVICE_VAL),
 };
 
 void mtu_updated(struct bt_conn *conn, uint16_t tx, uint16_t rx)
@@ -70,11 +107,20 @@ static void connected(struct bt_conn *conn, uint8_t err)
     } else {
         printk("Connected\n");
     }
+
+    if (!default_conn) {
+		default_conn = bt_conn_ref(conn);
+	}
 }
 
 static void disconnected(struct bt_conn *conn, uint8_t reason)
 {
     printk("Disconnected (reason 0x%02x)\n", reason);
+
+    if (default_conn) {
+		bt_conn_unref(default_conn);
+		default_conn = NULL;
+	}
 }
 
 static void alert_stop(void)
@@ -146,21 +192,10 @@ static struct bt_conn_auth_cb auth_cb_display = {
     .cancel = auth_cancel,
 };
 
-static void bas_notify(void)
-{
-    uint8_t battery_level = bt_bas_get_battery_level();
-
-    battery_level--;
-
-    if (!battery_level) {
-        battery_level = 100U;
-    }
-
-    bt_bas_set_battery_level(battery_level);
-}
-
 void main(void)
 {
+    struct bt_gatt_attr *vnd_ind_attr;
+    char str[BT_UUID_STR_LEN];
     int err;
     uint32_t dtr = 0;
     const struct device *const dev = DEVICE_DT_GET_ONE(st_vl53l0x);
@@ -184,6 +219,12 @@ void main(void)
 
     bt_gatt_cb_register(&gatt_callbacks);
     bt_conn_auth_cb_register(&auth_cb_display);
+
+    vnd_ind_attr = bt_gatt_find_by_uuid(vnd_svc.attrs, vnd_svc.attr_count,
+                        &vnd_enc_uuid.uuid);
+
+    bt_uuid_to_str(&vnd_enc_uuid.uuid, str, sizeof(str));
+    printk("Indicate VND attr %p (UUID %s)\n", vnd_ind_attr, str);
 
     if (!device_is_ready(dev)) {
         printk("sensor: device not ready.\n");
@@ -217,13 +258,21 @@ void main(void)
         float val = sensor_value_to_double(&value);
         uint16_t valint = val * 10000;
 
-        printk("distance is %hu\n", valint);
+        printk("distance is %d\n", valint);
 
         /* Heartrate measurements simulation */
-        bt_hrs_notify(10);
+        bt_hrs_notify(69);
 
-        /* Battery level simulation */
-        bas_notify();
+        // ind_params.attr = vnd_ind_attr;
+        // ind_params.func = indicate_cb;
+        // ind_params.destroy = indicate_destroy;
+        // ind_params.data = &valint;
+        // ind_params.len = sizeof(valint);
 
+        // bt_gatt_indicate(NULL, &ind_params);
+        ret = bt_gatt_notify(default_conn, &vnd_svc.attrs[1], &valint, sizeof(valint));
+        if(ret) {
+            printk("%d\n", ret);
+        }
     }
 }
